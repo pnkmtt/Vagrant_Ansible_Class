cb78bd82edb505f45881043541ce48611061f2fb
pnkmtt <pnkmtt@gmail.com>
2018-02-09 13:52:31 -0500

    updated message
    update sql insert of apache message

--- roles/mariadb/files/dump.sql
+++ roles/mariadb/files/dump.sql
@@ -2,4 +2,4 @@ CREATE TABLE IF NOT EXISTS demo (
   message varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-INSERT INTO demo (message) VALUES('Hello World!');
+INSERT INTO demo (message) VALUES('Hello World from mariaDB!');
